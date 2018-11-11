bb56a20a46785baf27edbded1b9ebf362ebb070e
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2018-11-11 21:36:18 +0200

    Add Galois multiplication, byte substitution, shift rows.

--- src/main/kotlin/com/arturs/stafanovics/crypto/BitSetExtensions.kt
+++ src/main/kotlin/com/arturs/stafanovics/crypto/BitSetExtensions.kt
@@ -2,5 +2,12 @@ package com.arturs.stafanovics.crypto
 
 import java.util.*
 
-fun BitSet.toHexString() = this.toLongArray()[0].toULong().toString(16).padStart(16, '0')
-fun BitSet.toBinaryString() = this.toLongArray()[0].toULong().toString(2).padStart(64, '0')
+@ExperimentalUnsignedTypes
+fun BitSet.toHexString() = this.toLongArray().foldRight("") {
+    elem, res -> res + elem.toULong().toString(16).padStart(16, '0')
+}
+
+@ExperimentalUnsignedTypes
+fun BitSet.toBinaryString() = this.toLongArray().foldRight(""){
+    elem, res -> res + elem.toULong().toString(2).padStart(64, '0')
+}
--- src/main/kotlin/com/arturs/stafanovics/crypto/crypto/aes/Aes.kt
+++ src/main/kotlin/com/arturs/stafanovics/crypto/crypto/aes/Aes.kt
@@ -1,279 +1,124 @@
 package com.arturs.stafanovics.crypto.crypto.aes
 
-import java.lang.NumberFormatException
 import java.util.*
+import kotlin.experimental.xor
+
 
 data class AesState(val stage: String, val key: BitSet?, val mes: BitSet)
 
 @ExperimentalUnsignedTypes
 class Aes {
 
-    // All permutation and substitution tables
     companion object {
-        private val circularShiftTable = intArrayOf(1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1)
-
-        private val compressionPermutationTable =
-                intArrayOf(14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
-                        23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
-                        41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-                        44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
-                ).reversedArray()
-
-        private val expansionPermutationTable =
-                intArrayOf(32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9,
-                        8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
-                        16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
-                        24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1
-                ).reversedArray()
-
-        private val sBoxTable =
-                listOf(
-                        intArrayOf(
-                                14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
-                                0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
-                                4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
-                                15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
-                        ),
-                        intArrayOf(
-                                15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
-                                3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
-                                0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
-                                13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
-                        ),
-                        intArrayOf(
-                                10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
-                                13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
-                                13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
-                                1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
-                        ),
-                        intArrayOf(
-                                7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
-                                13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
-                                10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
-                                3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14
-                        ),
-                        intArrayOf(
-                                2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
-                                14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
-                                4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
-                                11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
-                        ),
-                        intArrayOf(
-                                12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
-                                10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
-                                9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
-                                4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
-                        ),
-                        intArrayOf(
-                                4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
-                                13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
-                                1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
-                                6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
-                        ),
-                        intArrayOf(
-                                13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
-                                1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
-                                7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
-                                2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
-                        )
-                ).reversed()
-
-        private val pBoxTable =
-                intArrayOf(16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
-                        2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25
-                ).reversedArray()
-
-        private val initialPermutation=
-                intArrayOf(
-                        58, 50, 42, 34, 26, 18, 10, 2,
-                        60, 52, 44, 36, 28, 20, 12, 4,
-                        62, 54, 46, 38, 30, 22, 14, 6,
-                        64, 56, 48, 40, 32, 24, 16, 8,
-                        57, 49, 41, 33, 25, 17, 9, 1,
-                        59, 51, 43, 35, 27, 19, 11, 3,
-                        61, 53, 45, 37, 29, 21, 13, 5,
-                        63, 55, 47, 39, 31, 23, 15, 7
-                ).reversedArray()
-
-        private val finalPermutation =
-                intArrayOf(
-                        40, 8, 48, 16, 56, 24, 64, 32,
-                        39, 7, 47, 15, 55, 23, 63, 31,
-                        38, 6, 46, 14, 54, 22, 62, 30,
-                        37, 5, 45, 13, 53, 21, 61, 29,
-                        36, 4, 44, 12, 52, 20, 60, 28,
-                        35, 3, 43, 11, 51, 19, 59, 27,
-                        34, 2, 42, 10, 50, 18, 58, 26,
-                        33, 1, 41, 9, 49, 17, 57, 25
-                ).reversedArray()
+        private val sBox = intArrayOf(
+                0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+                0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+                0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+                0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+                0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+                0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+                0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+                0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+                0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+                0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+                0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+                0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+                0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+                0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+                0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+                0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+        )
+        private val invSbox = intArrayOf(
+                0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+                0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+                0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+                0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+                0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+                0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+                0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+                0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+                0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+                0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+                0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+                0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+                0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+                0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+                0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+                0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+        )
     }
 
     val history = mutableListOf<AesState>()
 
-    fun encrypt(key: String, message: String) = runRounds(key, message, revKeys = false)
-
-    // For decryption use reversed keys
-    fun decrypt(key: String, message: String) = runRounds(key, message, revKeys = true)
-
-    private fun runRounds(k: String, m: String, revKeys: Boolean): String {
-        history.clear()
-        history.add(AesState("Initial values", getBitSet(k), getBitSet(m)))
-
-        val keys = getSubKeys(getBitSet(k)).let {
-            if (!revKeys) it else it.reversed()
-        }
-
-        var message = getBitSet(m)
-        message = message.initialPermutation()
-        history.add(AesState("Initial permutation", null, message))
-
-        // Split message to left and right parts each 32 bit
-        var (left, right) = message.get(32, 64) to message.get(0, 32)
-        for (round in 0..15) {
-            val newLeftRight = performRound(keys[round], left, right)
-            left = newLeftRight.first
-            right = newLeftRight.second
-            history.add(AesState("Round ${round + 1}", keys[round], combineBitSets(left, right)))
-        }
-
-        // swap before final permutation
-        message = combineBitSets(left = right, right = left)
-        history.add(AesState("Swap halves", null, message))
-
-        message = message.finalPermutation()
-        history.add(AesState("Final permutation", null, message))
-        return message.toHexString()
+    fun encrypt(key: String, message: String): String {
+        val message = getByteArray(message)
+        substitution(message)
+        shiftRows(message)
+        return BitSet.valueOf(message).toHexString()
     }
 
-    private fun combineBitSets(left: BitSet, right: BitSet): BitSet {
-        val combined = (left.toLongArray()[0] shl 32) or right.toLongArray()[0]
-        return BitSet.valueOf(longArrayOf(combined))
+    private fun shiftRows(message: ByteArray) {
+        val twoDim = message.toTwoDimensional()
+        (1..3).forEach { twoDim[it] = twoDim[it].shiftRow(it) }
+        val oneDim = twoDim.toOneDimensional()
+        oneDim.copyInto(message)
     }
 
+    private fun ByteArray.shiftRow(count: Int) = (this.drop(count) + this.take(count)).toByteArray()
 
-    // Gets subkeys for each round
-    private fun getSubKeys(key: BitSet): List<BitSet> {
-        val list = mutableListOf<BitSet>()
-        for (i in 0..15) {
-            key.keyShift(i)
-            list.add(BitSet.valueOf(key.toLongArray()))
-        }
-        return list
-    }
-
-    // Parse hex or binary string and return BitSet representation of it
-    private fun getBitSet(str: String): BitSet {
-        val asLong = try {
-            str.toULong(16)
-        } catch (e: NumberFormatException) {
-            str.toULong(2)
-        }.toLong()
-
-        return BitSet.valueOf(longArrayOf(asLong))
-    }
-
-    // Run 1 DES round, on key and message halves, return new message halves
-    private fun performRound(key: BitSet, left: BitSet, right: BitSet): Pair<BitSet, BitSet> {
-        val fDRight = f(key.compressionPermutation(), right)
-        left.xor(fDRight)
-        // Swap halves
-        return right to left
-    }
-
-    private fun f(key: BitSet, right: BitSet): BitSet {
-        val expandedRight = right.expansionPermutation()
-        expandedRight.xor(key)
-        val sboxed = expandedRight.sBoxSubstition()
-        return sboxed.pBoxPermutation()
-    }
-
-    // Shift key bits, 1 or 2 times depending on round
-    private fun BitSet.keyShift(round: Int) {
-        this.circularShift()
-        if (circularShiftTable[round] == 2) this.circularShift()
-    }
-
-    // Do left circular shift on two key halves - each 28 bit
-    private fun BitSet.circularShift() {
-        val leftCarry = this[55]
-        val rightCarry = this[27]
-        val shifted = ((this.toLongArray()[0]) shl 1) and 0xFFFFFFFFFFFFFF
-        val bs = BitSet.valueOf(longArrayOf(shifted))
-        bs[0] = rightCarry
-        bs[28] = leftCarry
-        this.clear()
-        this.or(bs)
-    }
-
-    // Do bit the substitution on a 32 bit set
-    private fun BitSet.sBoxSubstition(): BitSet {
-        val bs = BitSet(32)
-        for (whichSBox in 0..7) {
-            val sBoxIndex = this.getSBoxIndex(whichSBox * 6)
-            // Retrieve substitution bits
-            val bits = sBoxTable[whichSBox][sBoxIndex]
-            bs.setSBoxBits(whichSBox, bits)
-        }
-        return bs
-    }
-
-    private fun BitSet.setSBoxBits(offset: Int, bits: Int) {
-        var remainingBits = bits
-        (0..3).forEach {
-            this[offset * 4 + it] = (remainingBits and 1) == 1
-            remainingBits = remainingBits shr 1
+    private fun substitution(stateArray: ByteArray) {
+        stateArray.forEachIndexed { index, elem ->
+            val row = elem.toInt() shr 4 and 0b1111
+            val col = elem.toInt() and 0b1111
+            stateArray[index] = sBox[row * 16 + col].toByte()
         }
     }
 
-    // Get index SBox substitution index
-    private fun BitSet.getSBoxIndex(offset: Int): Int {
-        // 6th and 1st bit
-        val row = (this[offset + 5].toInt() shl 1) + this[offset].toInt()
-        // 5th down to 2nd bit
-        val column = (offset + 4 downTo offset + 1).fold(0) { acc, ind -> (acc shl 1) + this[ind].toInt() }
-        return row * 16 + column
+    private infix fun Byte.galoisMult(by: Byte): Byte {
+        var currXtime = this.xtime()
+        val xtimesArr = ByteArray(7) {
+            val toReturn = currXtime
+            currXtime = currXtime.xtime()
+            toReturn
+        }
+
+        var sum = if (by.toInt() and 1 == 1) this else 0
+        (1..7).forEach {
+            if ((by.toInt() ushr it) and 1 == 1) {
+                sum = sum xor xtimesArr[it - 1]
+            }
+        }
+        return sum
     }
 
-    private fun BitSet.finalPermutation() = performPermutation(this, finalPermutation, 64)
-    private fun BitSet.initialPermutation() = performPermutation(this, initialPermutation, 64)
-    private fun BitSet.pBoxPermutation() = performPermutation(this, pBoxTable, 32)
-    // Expand 32 bits to 48
-    private fun BitSet.expansionPermutation() = performPermutation(this, expansionPermutationTable, 32)
+    private fun Byte.xtime() = ((this.toInt() shl 1) xor ((this.toInt() ushr 7) and 0x1b)).toByte()
 
-    // Compress 56 bits to 48
-    private fun BitSet.compressionPermutation() = performPermutation(this, compressionPermutationTable, 56)
+    private fun ByteArray.toTwoDimensional() = Array(4) { row ->
+        ByteArray(4) { col -> this[row + 4 * col] }
+    }
 
-    private fun performPermutation(bitSet: BitSet, permutations: IntArray, size: Int) = BitSet(64).also { newBs ->
-        permutations.forEachIndexed { ind, perm -> newBs[ind] = bitSet[size - perm] }
+    private fun Array<ByteArray>.toOneDimensional() = ByteArray(16) { this[it / 4][it % 4] }
+
+
+    // Parse hex string and return BitSet representation of it
+    private fun getByteArray(str: String): ByteArray {
+        val left = str.substring(0..15).toULong(16).toLong()
+        val right = str.substring(16..31).toULong(16).toLong()
+        return BitSet.valueOf(longArrayOf(right, left)).toByteArray().reversedArray()
     }
 
     private fun Boolean.toInt() = if (this) 1 else 0
 }
 
 @ExperimentalUnsignedTypes
-fun BitSet.toHexString() = this.toLongArray()[0].toULong().toString(16).padStart(16, '0')
+fun BitSet.toHexString() = this.toLongArray().foldRight("") { elem, res ->
+    res + elem.toULong().toString(16).padStart(16, '0')
+}
 
 @ExperimentalUnsignedTypes
 fun main() {
-    print("Key: ")
-    val key = "F0CCAAF556678F"
-    print("Message: ")
-    val message = "0123456789ABCDEF"
+    val key = "01FFFFFFFFFFFFFFFFFFFFFFFFFFFF01"
+    val message = "3243f6a8885a308d313198a2e0370734"
     val aes = Aes()
-
     val encryptedMessage = aes.encrypt(key, message)
-    println("Encrpyted message: $encryptedMessage")
-    println("Encryption steps: ")
-    println("Stage | Key | Message")
-    aes.history.forEach {
-        println("${it.stage} | ${it.key?.toHexString() ?: ""} | ${it.mes.toHexString()}")
-    }
-
-    println()
-    println("Decrypted message: ${aes.decrypt(key, encryptedMessage)}")
-    println("Decryption steps: ")
-    println("Stage | Key | Message")
-    aes.history.forEach {
-        println("${it.stage} | ${it.key?.toHexString() ?: ""} | ${it.mes.toHexString()}")
-    }
 }
